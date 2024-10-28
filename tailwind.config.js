ee3dc931a12e0ba2f379f318060572b5ad4534f7
Virtual Jasper <jasper2virtual@hotmail.com>
2024-10-28 23:16:08 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
